77d26bc69ce5ea8b9704e1c7175db7549658c652
Tom MacWright <tom@macwright.org>
2015-01-06 17:28:41 -0500

    Add documentation

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+/**
+ * Generates a new GeoJSON Point feature, given coordinates
+ * and, optionally, properties.
+ *
+ * @module turf/point
+ * @param {number} longitude - position west to east in decimal degrees
+ * @param {number} latitude - position south to north in decimal degrees
+ * @param {Object} properties
+ * @return {GeoJSONPoint} output
+ * @example
+ * var pt1 = turf.point(-75.343, 39.984)
+ */
 module.exports = function(x, y, properties){
   if(x instanceof Array) {
   	properties = y;
